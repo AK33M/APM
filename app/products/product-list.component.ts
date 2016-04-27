dd2a1ad2ce9989c5c74c5be5f4bda94fc961c64b
Akeem Taiwo (Work) <akeem.taiwo@dataireland.ie>
2016-04-27 17:02:27 +0100

    data binding and pipes'

--- app/products/product-list.component.ts
+++ app/products/product-list.component.ts
@@ -10,6 +10,10 @@ import {Component} from 'angular2/core';
 
 export class ProductListComponent {
     pageTitle: string = 'Product List';
+    imageWidth: number = 50;
+    imageMargin: number = 2;
+    showImage: boolean = false;
+    listFilter: string = 'cart';
     products: any[] = [
     {
         "productId": 1,
@@ -62,4 +66,8 @@ export class ProductListComponent {
         "imageUrl": "http://openclipart.org/image/300px/svg_to_png/120337/xbox-controller_01.png"
     }
 ];
+
+toggleImage(): void{
+    this.showImage = !this.showImage;
+}
 }
